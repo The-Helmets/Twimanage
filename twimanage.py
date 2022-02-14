715e2a4b2dd3e88783725b3f75ff51a0b5174c35
Dinesh Kumar <dseera6@gmail.com>
2022-02-14 23:34:09 +0530

    credentials regenerated

--- twimanage.py
+++ twimanage.py
@@ -1,10 +1,17 @@
 import tweepy
 
-credentials = {"api_key": "bajjVZ8pfi9z8Vo247p3y4fAA", "api_secret": "IFQjPXf1YsbYzOyaeR0mFgmEULMAyOrdvog9Q4EVXJZ3KVGiHb", "access_token": "1216406244358901760-kD4x5rZamNTHVCWElT2DrMqPwUStl2", "access_token_secret": "QDMqyejEwps1l8SyKThZtweEFj6uN6XNuMmMVKdQ9gjbX"}
+consumer_key = "QS9Vk6mSmKTri97Oz4yNuY3sH"
+consumer_secret = "szyiAlz9d0i5PmGEEEkukeyReDYHrFEgaMW2kTkytaqI1W2lH0"
+access_token = "1216406244358901760-m4oWrplywn1qx35Tr9dggLhQMq0ABZ"
+access_token_secret = "Srgnz57Kwda6gQie9iJFb3VuDVYSmj75shqwHsBPhQygu"
 
-auth = tweepy.OAuthHandler(credentials["api_key"], credentials["api_secret"])
-auth.set_access_token(credentials["access_token"], credentials["access_token_secret"])
+auth = tweepy.OAuth1UserHandler(
+    consumer_key, consumer_secret, access_token, access_token_secret
+)
+
 api = tweepy.API(auth)
 
-tweet = 'Hello, world!'
-api.update_status(status=tweet)
+print(api.verify_credentials().screen_name)
+
+#tweet = 'Hello, world!'
+#api.update_status(status=tweet)
