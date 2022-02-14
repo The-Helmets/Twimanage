fca496dc1732c8da87225e271459830d1ed308cd
Dinesh Kumar <dseera6@gmail.com>
2022-02-14 14:09:14 +0530

    added credentials

--- twimanage.py
+++ twimanage.py
@@ -1,1 +1,3 @@
 import tweepy
+
+credentials = {"api_key": "bajjVZ8pfi9z8Vo247p3y4fAA", "api_secret": "IFQjPXf1YsbYzOyaeR0mFgmEULMAyOrdvog9Q4EVXJZ3KVGiHb", "access_token": "1216406244358901760-kD4x5rZamNTHVCWElT2DrMqPwUStl2", "access_token_secret": "QDMqyejEwps1l8SyKThZtweEFj6uN6XNuMmMVKdQ9gjbX"}
