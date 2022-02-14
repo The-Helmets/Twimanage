2ed8f931a4f2f3faa08cce599598ea9ef007b757
elshivani <shivanithakurst0799@gmail.com>
2022-02-14 15:14:09 +0530

    getting the twitter api authentication

--- twimanage.py
+++ twimanage.py
@@ -1,3 +1,10 @@
 import tweepy
 
 credentials = {"api_key": "bajjVZ8pfi9z8Vo247p3y4fAA", "api_secret": "IFQjPXf1YsbYzOyaeR0mFgmEULMAyOrdvog9Q4EVXJZ3KVGiHb", "access_token": "1216406244358901760-kD4x5rZamNTHVCWElT2DrMqPwUStl2", "access_token_secret": "QDMqyejEwps1l8SyKThZtweEFj6uN6XNuMmMVKdQ9gjbX"}
+
+auth = tweepy.OAuthHandler(credentials["api_key"], credentials["api_secret"])
+auth.set_access_token(credentials["access_token"], credentials["access_token_secret"])
+api = tweepy.API(auth)
+
+tweet = 'Hello, world!'
+api.update_status(status=tweet)
