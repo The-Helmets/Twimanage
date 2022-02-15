5e22d12725f430356fbc0460ff9c24b09da33ef0
Dinesh Kumar <dseera6@gmail.com>
2022-02-16 01:36:06 +0530

    Refactor 2.0

---
+++ auth.py
@@ -0,0 +1,18 @@
+import tweepy
+
+consumer_key = "QS9Vk6mSmKTri97Oz4yNuY3sH"
+consumer_secret = "szyiAlz9d0i5PmGEEEkukeyReDYHrFEgaMW2kTkytaqI1W2lH0"
+callback= "oob"
+
+oauth1_user_handler = tweepy.OAuth1UserHandler(
+    consumer_key, consumer_secret,
+    callback)
+
+print(oauth1_user_handler.get_authorization_url(access_type='write'))
+
+verifier = input("Input PIN: ")
+access_token, access_token_secret = oauth1_user_handler.get_access_token(verifier)
+
+api = tweepy.API(oauth1_user_handler)
+
+print(api.verify_credentials().screen_name)
---
+++ manage.py
@@ -0,0 +1,8 @@
+def new_tweeet(text):
+    text = "Hello, world!"
+    api.update_status(status = text)
+
+
+
+
+
