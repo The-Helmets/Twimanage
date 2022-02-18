1db0317d9c539d03b3439d962dc46dbf1dd71d0f
Dinesh Kumar <dseera6@gmail.com>
2022-02-19 01:19:07 +0530

    our first working prototype
    Co-authored-by: qrshivani <shivani07999@gmail.com>

--- twimanage.py
+++ twimanage.py
@@ -1,1 +1,32 @@
 import tweepy
+import yaml
+
+from auth import *
+from manage import *
+
+
+print("Welcome to Twimanage! This is a simple Twitter management tool. /n")
+
+# Load credentials and authenticate
+try:
+    api = auth()
+    print("Authentication successful!")
+
+except:
+    print("Authentication failed! Please check your credentials.yaml file.")
+    exit()
+
+user_choice = (int(input("1. New Tweet\n2. Delete Tweet(Needs Tweed ID)\n3. Quit\n>>>")))
+if user_choice == 1:
+    tweet = input("What would you like to tweet? ")
+    new_tweeet(tweet, api)
+    print("Tweet sent!")
+
+elif user_choice == 2:
+    id = input("What is the ID of the tweet you want to delete? ")
+    del_tweet(id, api)
+    print("Tweet deleted!")
+
+elif user_choice == 3:
+    print("Goodbye!")
+    exit()
