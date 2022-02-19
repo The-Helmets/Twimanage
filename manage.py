3cf1ebe1fc44635c6d77e6b2ab9dc69322ddb526
elshivani <shivanithakurst0799@gmail.com>
2022-02-19 23:56:31 +0530

    adding tweet functionality

--- manage.py
+++ manage.py
@@ -1,6 +1,24 @@
 def new_tweeet(text, api):
-    api.update_status(status = text)
-
-def del_tweet(id, api):
-    api.destroy_status(id)
+    try:
+        api.update_status(status=text)
+        print( "Successfully tweeted ")
+    except:
+        print("Unsuccessfull ")
   
+    
+def del_tweet(id, api):
+    try:
+        api.destroy_status(id)
+        print("Successfully deleted " , id)
+    except:
+        print("Unsuccessfull" , id)
+
+def get_tweets(username, api):
+        number_of_tweets=1000
+        tweets = api.user_timeline(username)
+        tmp=[] 
+  
+        tweets_for_csv = [tweet.text for tweet in tweets]  
+        for x in tweets_for_csv:
+            tmp.append(x) 
+            print(tmp)
