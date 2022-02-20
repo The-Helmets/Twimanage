1ac7ff898e6f2c9ae56ee7ea471a908d84af19bc
Dinesh Kumar <dseera6@gmail.com>
2022-02-21 00:24:02 +0530

    added tweets grab
    Co-Authored-By: Shivani Thakur <91600746+qrshivani@users.noreply.github.com>

--- manage.py
+++ manage.py
@@ -1,3 +1,5 @@
+import yaml
+
 def new_tweeet(text, api):
     try:
         api.update_status(status=text)
@@ -5,7 +7,7 @@ def new_tweeet(text, api):
     except:
         print("Unsuccessfull ")
   
-    
+
 def del_tweet(id, api):
     try:
         api.destroy_status(id)
@@ -13,11 +15,23 @@ def del_tweet(id, api):
     except:
         print("Unsuccessfull" , id)
 
-def get_tweets(username, api):
-        tweets = api.user_timeline(username)
-        tmp=[] 
-  
-        tweets_for_csv = [tweet.text for tweet in tweets]  
-        for x in tweets_for_csv:
-            tmp.append(x) 
-            print(tmp)
+def get_tweets(user_id, api, count):
+    count = int(input("How many tweets would you like to grab? "))
+    for i in range(count):
+        tweets = api.user_timeline(screen_name = user_id, count = count, exclude_replies=True, include_rts = False)
+    
+    grabbed_tweets = {}
+    i = 1
+
+    for tweet in tweets:
+        j = str("tweet_" + str(i))
+        grabbed_tweets[j] = {}
+        grabbed_tweets[j]['id'] = tweet.id
+        grabbed_tweets[j]['text'] = tweet.text
+        grabbed_tweets[j]['name'] = tweet.user.name
+        i += 1
+    
+    with open("Data/grabbed_tweets.yaml", "w") as f:
+        f.write(yaml.dump(grabbed_tweets, sort_keys=False, default_flow_style=False))
+
+    print("Tweets saved to Data/grabbed_tweets.yaml")
