206d0be59784d2114112b00057ece769700eb387
Dinesh Kumar <dseera6@gmail.com>
2022-02-21 00:24:02 +0530

    added tweets grab
    Co-Authored-By: Shivani Thakur <91600746+qrshivani@users.noreply.github.com>

--- auth.py
+++ auth.py
@@ -9,17 +9,21 @@ def auth():
         consumer_key, consumer_secret,
         callback)
 
-    print("Visit this link to generate 6-digit PIN: ", oauth1_user_handler.get_authorization_url(access_type='write'))
+    print("Visit this link to generate 7-digit PIN: ", oauth1_user_handler.get_authorization_url(access_type='write'))
 
     verifier = input("Input PIN: ")
     access_token, access_token_secret = oauth1_user_handler.get_access_token(verifier)
 
     with open("credentials.yaml", "w") as f:
+        f.write("consumer_key: {}\n".format(consumer_key))
+        f.write("consumer_secret: {}\n".format(consumer_secret))
         f.write("access_token: {}\n".format(access_token))
         f.write("access_token_secret: {}\n".format(access_token_secret))
 
     api = tweepy.API(oauth1_user_handler)
-    user_name = api.verify_credentials().screen_name
-    print("Signed in as: ", user_name)
+    user_name = api.verify_credentials().id_str
+    user_id = api.verify_credentials().id_str
+    print("Signed in as: ", user_name, " User ID: ", user_id)
 
-    return api, user_name
+
+    return api, user_name, user_id
--- twimanage.py
+++ twimanage.py
@@ -1,9 +1,9 @@
+from itertools import count
 import tweepy
 import yaml
 
 from auth import *
 from manage import *
-
 
 print("Welcome to Twimanage! This is a simple Twitter management tool.")
 
@@ -15,27 +15,29 @@ try:
     auth_1 = tweepy.OAuthHandler(credentials['consumer_key'], credentials['consumer_secret'])
     auth_1.set_access_token(credentials['access_token'], credentials['access_token_secret'])
     api = tweepy.API(auth_1)
-    print("Authentication successful!")
+    user_name = api.verify_credentials().screen_name
+    user_id = api.verify_credentials().id_str
+    print("Signed in as: ", user_name, " User ID: ", user_id)
     
 except:
-    if input("Credentials not found. Do you want to sign in using 3 PIN Authentication? (y/n): ") == "y":
-        api, user_name = auth()
+    if input("Credentials not found. Do you want to sign in us3ing 3 PIN Authentication? (y/n): ") == "y":
+        api, user_name, user_id = auth()
         print("3 PIN Authentication successful!")
+        print(user_name)
     else:
         print("Authentication failed! Please check your credentials.yaml file.")
         exit()
 
 # Main menu
 while True:
-    user_choice = (int(input("What would you like to do?\n1. New Tweet\n2. Delete Tweet(Needs Tweet ID)\n3. Quit\n>>>")))
+    user_choice = (int(input("What would you like to do?\n1. New Tweet\n2. Get Tweet\n3. Quit\n>>>")))
     if user_choice == 1:
         tweet = input("What would you like to tweet? ")
         new_tweeet(tweet, api)
         print("Tweet sent!")
 
     elif user_choice == 2:
-        get_tweets(user_name, api)
-        print("Tweet deleted!")
+        get_tweets(user_name, api, count)
 
     elif user_choice == 3:
         print("Goodbye!")
